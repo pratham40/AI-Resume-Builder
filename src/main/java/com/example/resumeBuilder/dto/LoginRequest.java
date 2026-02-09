cdc0db735f6ed1fa3721d2914bca46fb1a920dc0
pratham40 <pratham55575@gmail.com>
2026-02-09 16:31:23 +0530

    add LoginRequest.java Dto

--- src/main/java/com/example/resumeBuilder/dto/LoginRequest.java
+++ src/main/java/com/example/resumeBuilder/dto/LoginRequest.java
@@ -1,4 +1,22 @@
 package com.example.resumeBuilder.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequest {
+
+    @NotBlank(message = "email is required")
+    @Email(message = "email should be valid")
+    private String email;
+
+    @NotBlank(message = "password is required")
+    private String password;
 }
